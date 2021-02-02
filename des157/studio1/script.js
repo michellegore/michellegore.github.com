301a201daccd45f3bd29c082eb4670302f5edcbb
Michelle Gore <michellegore5@gmail.com>
2021-02-01 22:36:21 -0800

    Added madlibs

--- des157/studio1/script.js
+++ des157/studio1/script.js
@@ -1,24 +1,29 @@
 (function(){
     'use strict';
 
-    var myForm = document.querySelector('#myform');
-    var madlib = document.querySelector('#madlib');
+    const myForm = document.querySelector('#myform');
+    const madlib = document.querySelector('#madlib');
 
     myForm.addEventListener('submit', function(event){
         event.preventDefault();
-        var noun1 = document.querySelector('#noun1').value;
-        var noun2 = document.querySelector('#noun2').value;
-        var adjective = document.querySelector('#adjective').value;
-        var verb = document.querySelector('#verb').value;
+        const noun1 = document.querySelector('#noun1').value;
+        const noun2 = document.querySelector('#noun2').value;
+        const verb1 = document.querySelector('#verb1').value;
+        const adverb1 = document.querySelector('#adverb1').value;
+        const bodypart = document.querySelector('#bodypart').value;
+        const adverb2 = document.querySelector('#adverb2').value;
 
-        if(noun1 && noun2 && adjective && verb){
-            var myText = `${noun1} and ${noun2} are ${adjective} and are going to ${verb} today!`;
+        if(noun1 && noun2 && verb1 && adverb1 && bodypart && adverb2){
+            myForm.remove();
+            let myText = `You wake up at 8am and take ${noun1} out for their daily walk. As you are walking through ${noun2}, you notice there’s a new restaurant that popped up. ${noun1} suddenly starts ${verb1} in circles, and you know they’re about to do their thing. The owner of the new restaurant storms out and ${adverb1} shouts “THAT IS NOT ACCEPTABLE!” You shake your ${bodypart} and walk away...it didn’t bother you too much. <br><br>However, ${noun1} had a different opinion. ${noun1} was deeply offended. Now everyday when walking past the restaurant, ${noun1} looks ${adverb2} at the owner straight in the eyes and pees in front of the restaurant entrance. <br><br>Just because.`;
+            madlib.innerHTML = myText;
+            madlib.className = "output";
         }
         else{
-            var myText = "Please give me words to put in your Mad Lib!";
+            let myText = "<br>Please give me words to put in your Mad Lib!";
+            madlib.innerHTML = myText;
+            madlib.className = "error";
         };
-
-        madlib.innerHTML = myText;
 
     });
 
